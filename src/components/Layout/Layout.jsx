96b13ea7a28de18268e3d05ff67ebbef4bf86e13
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-08-27 20:37:15 +0200

    update_responsive_Layout_component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,26 @@
 import { Suspense } from "react";
-import Navigation from "../Navigation/Navigation";
+import Header from "../Header/Header";
 import css from "./Layout.module.css";
 import Loader from "../Loader/Loader";
 
+// export default function Layout({ children }) {
+//   return (
+//     <div className={css.containerBody}>
+//       <Header />
+//       <Suspense fallback={<Loader />} className={css.main}>
+//         {children}
+//       </Suspense>
+//     </div>
+//   );
+// }
+
 export default function Layout({ children }) {
   return (
-    <div className={css.container}>
-      <Navigation />
-      <Suspense fallback={<Loader />}>{children}</Suspense>
-    </div>
+    <>
+      <Header />
+      <main className={css.mainContainer}>
+        <Suspense fallback={<Loader />}>{children}</Suspense>
+      </main>
+    </>
   );
 }
